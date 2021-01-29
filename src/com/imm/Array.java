635b1459e0a44854c67508d6e6df8f83ef12678c
iranga <irangacomz@gmail.com>
2021-01-29 22:18:48 +0530

    Creating Insert method to Array

--- src/com/imm/Array.java
+++ src/com/imm/Array.java
@@ -3,13 +3,32 @@ package com.imm;
 //Creating Array Class to implement Array Functionalities
 public class Array {
     private int [] items;
+    private int count=0;
 
     public Array(int length){
         items = new int[length];
     }
 
+    public void insert(int item){
+        //if the array is full, resize it
+        if(items.length == count){
+            //create a new Array (twice the size)
+            int [] newItems = new int [count*2];
+
+            //copy all the existing items to new Array
+            for(int i=0; i< count; i++)
+                newItems[i]=items[i];
+
+            //ser 'items' to this new Array
+            items = newItems;
+
+        }
+        //add the new element at the end
+        items[count++]=item;
+    }
+
     public void print(){
-        for(int i=0; i< items.length; i++)
+        for(int i=0; i< count; i++)
             System.out.println(items[i]);
 
     }
--- src/com/imm/Main.java
+++ src/com/imm/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         //creating new Array
         Array numbers = new Array(3);
+        numbers.insert(1);
+        numbers.insert(2);
+        numbers.insert(1);
+        numbers.insert(2);
         numbers.print();
     }
 }
