3afaa0f6506fd9b725bba1b72da5ae227f6279e9
iranga <irangacomz@gmail.com>
2021-01-30 01:41:36 +0530

    implement addLast method to LinkedList

--- src/com/imm/LinkedList.java
+++ src/com/imm/LinkedList.java
@@ -1,13 +1,32 @@
 package com.imm;
 
-public class LinkedList {
+public class LinkedList  {
+
+    //make Node class private/ only accible by LinkedList class
+    private class Node {
+        private int value;
+        private Node next; //Store address to the next Node
+
+        public Node(int value){
+            this.value=value;
+        }
+
+    }
+
     private Node first;//head
     private Node last;//tail
 
-    //addFirst
-    //addLast
-    //deleteFirst
-    //deleteLast
-    //contains
-    //indexOf
+    public void addLast(int item){
+        var node = new Node(item);
+        //if linkedlist empty assign to first Node
+        if(first == null)
+            first = last = node;
+        //else append the node to last Node
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+
 }
--- src/com/imm/Main.java
+++ src/com/imm/Main.java
@@ -4,13 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         //creating new Array
-        Array numbers = new Array(3);
-        numbers.insert(1);
-        numbers.insert(2);
-        numbers.insert(3);
-        numbers.insert(4);
-        numbers.removeAt(1);
-        System.out.println(numbers.indexOf(1));
-        numbers.print();
+//        Array numbers = new Array(3);
+//        numbers.insert(1);
+//        numbers.insert(2);
+//        numbers.insert(3);
+//        numbers.insert(4);
+//        numbers.removeAt(1);
+//        System.out.println(numbers.indexOf(1));
+//        numbers.print();
+
+        //creating new LinkedList
+        var linkedList = new LinkedList();
+        linkedList.addLast(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
     }
 }
