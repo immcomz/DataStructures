f7d419e52a7cc32a32cbf1239a6c0a7b8a6461c4
iranga <irangacomz@gmail.com>
2021-01-30 00:59:29 +0530

    Initial Implementation to LinkedList DataStructure

---
+++ src/com/imm/LinkedList.java
@@ -0,0 +1,13 @@
+package com.imm;
+
+public class LinkedList {
+    private Node first;//head
+    private Node last;//tail
+
+    //addFirst
+    //addLast
+    //deleteFirst
+    //deleteLast
+    //contains
+    //indexOf
+}
---
+++ src/com/imm/Node.java
@@ -0,0 +1,6 @@
+package com.imm;
+//To implement LinkedList
+public class Node {
+    private int value;
+    private Node next; //Store address to the next Node
+}
