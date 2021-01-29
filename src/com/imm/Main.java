4a32e800159398f207431758a546c73108fae06e
iranga <irangacomz@gmail.com>
2021-01-29 21:58:33 +0530

    Creating Array Class to implement Array Functionalities

--- src/com/imm/Main.java
+++ src/com/imm/Main.java
@@ -3,6 +3,8 @@ package com.imm;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("asdsaa");System.out.println("asdsaa");System.out.println("asdsaa");
+        //creating new Array
+        Array numbers = new Array(3);
+        numbers.print();
     }
 }
